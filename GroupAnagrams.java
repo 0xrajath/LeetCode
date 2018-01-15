7982a2e318ff190de202e2494d38e53e90940c56
rajathalex <rajathalex@gmail.com>
2018-01-15 14:10:37 -0500

    Added Question to GroupAnagrams

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,3 +1,18 @@
+/* 
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case. 
+*/
+
+
 import java.util.*;
 
 class Solution {
